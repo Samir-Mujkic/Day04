e4fb83cfa670081ad9ff801eb5d8e443ea065323
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 13:20:12 +0100

    zadatak 10

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+    //Only change code below this code
+   var myFirstName ="Samir";
+   var myLastName ="Mujkic";
+   var myHomeTown ="Sarajevo";
+    
+    
+    //Only change code above this line
+    return{
+        myFirstName,
+        myLastName,
+        myHomeTown
+
+    }
+    
+}
+    console.log(main());
+    module.exports=main;
