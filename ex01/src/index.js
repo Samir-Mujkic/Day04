c72c1080ed17212849816d5573f07454f7f5b0b7
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 12:47:14 +0100

    zadatak 1

---
+++ ex01/src/index.js
@@ -0,0 +1,22 @@
+function main(numOne,numTwo){
+    //Only change code below this code
+    var numOne = numOne;
+    var numTwo = numTwo;
+    var quotient;
+    if(numTwo===0){
+
+     quotient= "divide by 0 ! Cannot divide by zero!" ;
+
+    }else {
+         quotient =numOne/numTwo;
+    }
+
+    //Only change code above this line
+    return quotient;
+}
+    console.log(main(15,3));
+    console.log(main(25,5));
+    console.log(main(420,10));
+    console.log(main(55,0));
+    module.exports=main;
+    
