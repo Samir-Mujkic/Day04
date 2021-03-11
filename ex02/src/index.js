1763b3ab6f09da88fe3c2f8b4d4d352d2b54bdda
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 10:32:14 +0100

    zadatak 2

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this code
+    var myRes = 75;
+    
+    myRes++;
+    //Only change code above this line
+    return myRes;
+}
+    console.log(main());
+    module.exports=main;
+ 
