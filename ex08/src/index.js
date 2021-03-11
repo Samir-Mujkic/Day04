20c1f115797f81ac4636b476d710767feb72128e
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 20:17:12 +0100

    zadatak 8

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+    //Only change code below this code
+    
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
+    
+    myX = myX + 3;
+    myY = myY + 17;
+    myZ = myZ -20;
+      //Only change code above this line
+    return {
+        myX,
+        myY,
+        myZ
+    };
+    
+    }
+      
+      console.log(main( ));
+       module.exports=main;
