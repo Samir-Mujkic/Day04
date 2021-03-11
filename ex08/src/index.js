888b4ce984525358538e8077e5c58bcf7722e4f7
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 20:19:20 +0100

    zadatak 8

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -4,10 +4,10 @@ function main(){
     var myX = 12;
     var myY = 19;
     var myZ = 24;
-    
-    myX = myX + 3;
-    myY = myY + 17;
-    myZ = myZ -20;
+
+    myX +=  3;
+    myY +=  17;
+    myZ -= 20;
       //Only change code above this line
     return {
         myX,
