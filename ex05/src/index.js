36b0a76406440963603de78868b6b521f76f95bc
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 17:30:47 +0100

    zadatak 5

---
+++ ex05/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne,numTwo){
+    //Only change code below this code
+    var numOne =  numOne ;
+    var  numTwo =   numTwo ;
+    var product =    numOne * numTwo;
+    
+
+    
+   
+    
+    //Only change code above this line
+    return product ;
+    }
+      
+     
+    
+
+    console.log(parseFloat(2.5*2.0 ));
+    console.log(parsefloat(5.5*5.5));
+    console.log(parsefloat(100.25*43.20));
+    module.exports=main;
