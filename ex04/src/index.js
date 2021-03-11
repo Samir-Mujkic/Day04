69ccefb49b50efe7e2bc8a97c9494b399e6b9696
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 11:27:59 +0100

    zadatak 4

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //Only change code below this code
+    var myDecimal = 82e-1
+    var myDecimale = 254e-5 
+    
+    
+    //Only change code above this line
+    return{
+        myDecimal,
+         myDecimale 
+    } 
+    
+}
+    console.log(main());
+    module.exports=main;
