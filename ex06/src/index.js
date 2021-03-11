356065d7ef99cf32162237f214d2a2be9d475970
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-11 17:45:24 +0100

    zadatak 6

---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne,numTwo){
+    //Only change code below this code
+    var numOne =  numOne ;
+    var  numTwo =   numTwo ;
+    var myQuotient =    numOne / numTwo;
+    
+
+    
+   
+    
+    //Only change code above this line
+    return myQuotient ;
+    }
+      
+     
+    
+
+    console.log(main(5,2 ));
+    console.log(main(16.5,5.5));
+    console.log(main(102.0,25.5));
+    module.exports=main;
